3c960fdf581f9f9a44ddfcf21d05fbaa28dc95dc
Bl1nker <k.alex.nsk@yandex.ru>
2025-03-20 19:05:26 +0700

    initial commit

---
+++ Chat_Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chat_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
